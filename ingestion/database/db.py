26d39174788921819df374b437b81c92bb88f431
LeithGharbi1 <leith.gharbi@esprit.tn>
2026-04-22 12:55:22 +0100

    render preparation

--- ingestion/database/db.py
+++ ingestion/database/db.py
@@ -7,4 +7,5 @@ def get_connection():
         database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT", "5432")
     )
